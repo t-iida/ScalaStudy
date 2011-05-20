9d126214165936172aa3aa5960152373c1b94b2e
Takuya Iida <dbiida@gmail.com>
2011-05-21 06:57:04 +0900

    Remove some needless code and add comment.

--- MongoBasic.scala
+++ MongoBasic.scala
@@ -1,9 +1,14 @@
+// Import every thing for MongoDB.
 import com.mongodb.{Mongo, DB, DBCollection, BasicDBObject, DBObject, DBCursor}
 
+// Import JavaConversions for implicit comversion.
+// Still those are NOT required in this code...
 import scala.collection.JavaConversions._
 
+// Create DBCollection.
 val dbCollection: DBCollection = new Mongo("localhost").getDB("mydb").getCollection("persons")
 
+// Function creates BasicDBObject by using parameter.
 def createDoc(i: Int): BasicDBObject = {
   val doc = new BasicDBObject
   doc.put("name", "name_" + i)
@@ -11,12 +16,13 @@ def createDoc(i: Int): BasicDBObject = {
   doc
 }
 
+// Create documents by using function defined above.
 val docs: Seq[BasicDBObject] = (0 to 9).map(i => createDoc(i))
 
+// Insert all documents created above.
 docs.foreach(doc => dbCollection.insert(doc))
 
-val cur: DBCursor = dbCollection.find()
-
+// Print documents ritrieved above by self recursion.
 def printCursor(cur: DBCursor): Unit = {
   if(cur.hasNext()) {
     println(cur.next)
@@ -24,6 +30,7 @@ def printCursor(cur: DBCursor): Unit = {
   }
 }
 
+// Invoke function with query without any condition.
 printCursor(dbCollection.find)
 
 
